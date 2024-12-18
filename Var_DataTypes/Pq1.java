2272d083f65ee14c30d36eb6c2cf801f28434898
YANA-TEAM <anonymous1436143@outlook.com>
2024-12-17 23:29:13 -0800

    Practice Question 1

---
+++ Var_DataTypes/Pq1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Pq1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number 1 :");
+        int num1 = sc.nextInt();
+        System.out.print("Enter Number 2 :");
+        int num2 = sc.nextInt();
+        System.out.print("Enter Number 3 :");
+        int num3 = sc.nextInt();
+        int average = (num1+num2+num3)/3;
+        System.out.println("Average is "+average);
+    }
+}
