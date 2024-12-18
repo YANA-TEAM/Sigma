dc739cc48dc514c3fd6e470feefb6a3e07bf6ff3
YANA-TEAM <anonymous1436143@outlook.com>
2024-12-17 23:38:28 -0800

    Practice Question 2 Added

---
+++ Var_DataTypes/Pq2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Pq2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Side of a Square :");
+        int side = sc.nextInt();
+        int areaOfSquare = (side*side);
+        System.out.println("Area of Square is "+areaOfSquare);
+    }
+}
