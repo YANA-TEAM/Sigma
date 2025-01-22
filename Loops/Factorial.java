34e7b8b31693d91b2c53610bfac9a36f9bf5caa4
YANA_TEAM <anonymous1436143@outlook.com>
2025-01-21 22:13:09 -0800

    Factorial java added

---
+++ Loops/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Factorial {
+    public static int factorial(int number) {
+        int fact = 1;
+        // Loop from 1 to the given number
+        for (int i = 1; i <= number; i++) {
+            fact *= i; // Multiply fact by i
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int input = sc.nextInt();  // Get the number from user input
+        // facts = new Factorial();
+        System.out.println("Factorial of " + input + " is " + factorial(input));
+    }
+}
