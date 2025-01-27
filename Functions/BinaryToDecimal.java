af85389133f3033b7030d121ee7d6c3434fbf0dc
YANA_TEAM <anonymous1436143@outlook.com>
2025-01-26 21:14:22 -0800

    BinaryToDecimal code added

---
+++ Functions/BinaryToDecimal.java
@@ -0,0 +1,16 @@
+public class BinaryToDecimal {
+    public static void main(String[] args){
+        int number = 101;
+        int power = 0;
+        int decimal = 0;
+        while(number > 0){
+            int lastDigit = number%10;
+            decimal = decimal + (lastDigit * (int)Math.pow(2, power));
+            number = number/10;
+            power++;
+        }
+        System.out.println(decimal);
+    }
+}
+
+
