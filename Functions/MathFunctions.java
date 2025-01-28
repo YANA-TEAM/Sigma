9a7d6c5d0ef551cd77f4559af1b78c98e2db95d7
YANA-TEAM <anonympus1436143@outlook.com>
2025-01-28 08:30:16 +0530

    MathFunctions added

---
+++ Functions/MathFunctions.java
@@ -0,0 +1,11 @@
+public class MathFunctions {
+    public static void main(String[] args) {
+        System.out.println("Math.abs(-26.7) = " + Math.abs(-26.7));
+        System.out.println("Math.ceil(7.4) = " + Math.ceil(7.4));
+        System.out.println("Math.floor(7.8) = " + Math.floor(7.8));
+        System.out.println("Math.max(8.6, 5.2) = " + Math.max(8.6, 5.2));
+        System.out.println("Math.min(8.6, 5.2) = " + Math.min(8.6, 5.2));
+        System.out.println("Math.pow(2, 3) = " + Math.pow(2, 3));
+        System.out.println("Math.sqrt(9) = " + Math.sqrt(9));
+    }
+}
