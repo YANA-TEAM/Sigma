f153f6516a28a6809c1f6483de805b3d99fe62fc
YANA-TEAM <anonympus1436143@outlook.com>
2025-02-03 21:38:05 +0530

    BinarySearch

--- Arrays/BinarySearch.java
+++ Arrays/BinarySearch.java
@@ -1,26 +1,29 @@
 public class BinarySearch {
-    public static int binarySearch(int arr[], int key){
-        int start = arr[0];
-        int end = arr.length;
-        for(int i = 0; i <= arr.length; i++){    
-            int mid = (start+end)/2;
-            if(arr[mid] == key){
-                return key;
+    public static int binarySearch(int arr[], int key) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == key) {
+                return mid;
             }
-            if(arr[mid] < key){
-                start += 1;
-            }
-            if(arr[mid] > key){
-                end-=1;
+            if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return -1;
-    }    
+    }
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7,8,9,10};
-        int key = 5;
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int key = 8;
         int index = binarySearch(arr, key);
-        System.out.println("key is founded at index :"+index);
+        if (index == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + index);
+        }
     }
 }
