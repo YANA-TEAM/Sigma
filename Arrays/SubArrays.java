2b85ecec037a8d89125d959e1e3a5559fda7a00c
YANA_TEAM <anonymous1436143@outlook.com>
2025-02-04 00:40:38 -0800

    updated

--- Arrays/SubArrays.java
+++ Arrays/SubArrays.java
@@ -1,29 +1,20 @@
 public class SubArrays {
     public static void subArrays(int arr[]){
-        int total = 0;
-        int maxSum = Integer.MIN_VALUE;
+        int totalSubArray = 0;
         for(int i = 0; i <= arr.length-1; i++){
             for(int j = i; j <= arr.length-1; j++){
-                int sum = 0;
                 for(int k = i; k <= j; k++){
                     System.out.print(arr[k]);
-                    sum += arr[k];
                 }
                 System.out.println();
-                System.out.println("Sum is :"+sum);
-                if(sum >= maxSum){
-                    maxSum = sum;
-                }
-                total++;
-            }
-            System.out.println();
+                totalSubArray++;
         }
-        System.out.println("Total subArrays are :"+total);
-        System.out.println("Maximum SubArray Sum is :"+maxSum);
-
+        System.out.println();
     }
+    System.out.println("Total subArrays are :"+totalSubArray);
+}
     public static void main(String[] args) {
-        int arr[] = {1,-2,6,-1,3};
+        int arr[] = {2,4,6,8,10};
         subArrays(arr);
     }
 }
